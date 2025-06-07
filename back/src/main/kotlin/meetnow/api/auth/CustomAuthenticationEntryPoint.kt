598ca395c697da06b234417ddd46cd7e96e7fc23
yeongwoooo <rdd9223@gmail.com>
2025-06-07 16:13:51 +0900

    feat: 스프링 시큐리티 적용
    - 요청시 헤더에 `X-API-KEY` 입력해야 함

---
+++ back/src/main/kotlin/meetnow/api/auth/CustomAuthenticationEntryPoint.kt
@@ -0,0 +1,24 @@
+package meetnow.api.auth
+
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.stereotype.Component
+
+@Component
+class CustomAuthenticationEntryPoint : AuthenticationEntryPoint {
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    override fun commence(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authException: AuthenticationException,
+    ) {
+        // 인증되지 않은 요청에 대한 로그를 기록합니다.
+        log.error("Unauthorized request: ${request.requestURI}, Exception: ${authException.message}")
+        // 인증되지 않은 요청에 대해 401 Unauthorized 응답을 반환합니다.
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
+    }
+}
