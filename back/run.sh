1f3dc75ca467253e99ff05bc83c9c170610494fb
WonHyeongCho <tmzkdls7989@naver.com>
2025-06-19 12:53:13 +0900

    계정 정보 수정

--- back/run.sh
+++ back/run.sh
@@ -1,6 +1,6 @@
 # 1. 환경 변수 설정
-export MONGO_DB_USERNAME="meetnow"
-export MONGO_DB_PASSWORD="Nol-universe.com2025"
+export MONGO_DB_USERNAME="Your Username"
+export MONGO_DB_PASSWORD="Your Password"
 
 # 2. Docker 빌드
 ./docker-build.sh
